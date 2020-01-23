d72281f64af4fc45348c0f70ff096fd7199278c0
sujasajeev <59528096+sujasajeev@users.noreply.github.com>
2020-01-23 12:45:34 +0530

    Add files via upload

---
+++ springdemo1/src/main/java/com/cts/springmaven/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.cts.springmaven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String getIndexPage() {
+		return "index";
+	}
+
+}
